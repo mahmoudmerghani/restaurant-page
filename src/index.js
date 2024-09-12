96fe64c10be697e487e7f3f7e64327ebafd84113
Mahmoud Mirghani <mahmoudtawfiq777@gmail.com>
2024-09-13 00:14:13 +0300

    Add selected state to buttons

--- src/index.js
+++ src/index.js
@@ -9,6 +9,18 @@ const homeBtn = document.getElementById("home");
 const menuBtn = document.getElementById("menu");
 const aboutBtn = document.getElementById("about");
 
-homeBtn.addEventListener("click", createInitialContent);
-menuBtn.addEventListener("click", createMenuContent);
-aboutBtn.addEventListener("click", createAboutContent);
+let selected = homeBtn;
+
+function changeContent(createContentFn, event) {
+    if (event.target === selected) return;
+
+    createContentFn();
+    
+    selected.classList.remove("selected");
+    selected = event.target;
+    selected.classList.add("selected");
+}
+
+homeBtn.addEventListener("click", e => changeContent(createInitialContent, e));
+menuBtn.addEventListener("click", e => changeContent(createMenuContent, e));
+aboutBtn.addEventListener("click", e => changeContent(createAboutContent, e));
